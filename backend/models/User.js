efe1899e618e558f306ac45e0047223382cad35a
Nishadh4014 <nishadhshah1249@gmail.com>
2024-07-18 00:08:38 +0530

    Schema updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,20 @@
+const { Double } = require('mongodb');
 const mongoose = require('mongoose');
+const { type } = require('os');
 
 const UserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
     mobileNumber: {
         type: String,
         required: true,
@@ -9,6 +23,43 @@ const UserSchema = new mongoose.Schema({
     otp: String,
     otpExpires: Date,
     passwordHash: String,
+    categories: [
+        {
+            type: String,
+            required: true
+        }
+    ],
+    status: {
+        type: Boolean
+    },
+    rating: {
+        type: Double
+    },
+    address: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    created_at: {
+        type: Date
+    },
+    updated_at: {
+        type: Date
+    },
+    appointments: [
+        {
+            type: String         // appointment id
+        }
+    ],
+    payments: [
+        {
+            type: String          // transaction ids
+        }
+    ]
 });
 
 module.exports = mongoose.model('User', UserSchema);
